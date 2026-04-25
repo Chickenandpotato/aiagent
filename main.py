5b29e5cf445d72fed0ff190a1fea33f6cab4d135
chickenandpotatoes <jjwang1018@gmail.com>
2026-04-24 21:59:30 -0700

    cleaned up code in main.py

--- main.py
+++ main.py
@@ -1,32 +1,51 @@
 import argparse
-parser = argparse.ArgumentParser(description="Chatbot")
-parser.add_argument("user_prompt", type=str, help="Enter prompt here")
-args = parser.parse_args()
-
 import os
+
+
 from dotenv import load_dotenv
-load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY")
-if api_key == None:
-    raise RuntimeError("Could not find API")
-
-
 from google import genai
+from google.genai import types
 
-client = genai.Client(api_key=api_key)
+
+def build_parser():
+    parser = argparse.ArgumentParser(description="Chatbot")
+    parser.add_argument("user_prompt", type=str, help="Enter prompt here")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
+    return parser
+
+
 
 def main():
-    print("Hello from aiagent!")
+    
+    parser = build_parser()
+    args = parser.parse_args()
+    
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    if api_key == None:
+        raise RuntimeError("Could not find API")
+    
+    client = genai.Client(api_key=api_key)
+    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
+    generate_content(client, messages, args.verbose)
+    
+    
+    
+    
+def generate_content(client, messages, verbose):
     response = client.models.generate_content(
-        model = "gemini-2.5-flash", contents = args.user_prompt
+        model = "gemini-2.5-flash",
+        contents = messages,
     )
     if response.usage_metadata == None:
         raise RuntimeError("failed API request")
-    print(
-        f"Prompt tokens: {response.usage_metadata.prompt_token_count}\nResponse tokens: {response.usage_metadata.candidates_token_count}"
-    )
     
+    if verbose:
+        print(
+            f"User prompt: {messages[0].parts[0].text}\nPrompt tokens: {response.usage_metadata.prompt_token_count}\nResponse tokens: {response.usage_metadata.candidates_token_count}"
+    )
     print(response.text)
+
 
 
 if __name__ == "__main__":
