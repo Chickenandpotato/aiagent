7c9839d259692a2a4cfbd671ec96fa246dad9097
chickenandpotatoes <jjwang1018@gmail.com>
2026-04-24 18:20:29 -0700

    adding in user entry parsing

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-
+import argparse
+parser = argparse.ArgumentParser(description="Chatbot")
+parser.add_argument("user_prompt", type=str, help="Enter prompt here")
+args = parser.parse_args()
 
 import os
 from dotenv import load_dotenv
@@ -15,13 +18,12 @@ client = genai.Client(api_key=api_key)
 def main():
     print("Hello from aiagent!")
     response = client.models.generate_content(
-        model = "gemini-2.5-flash", contents = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+        model = "gemini-2.5-flash", contents = args.user_prompt
     )
     if response.usage_metadata == None:
         raise RuntimeError("failed API request")
     print(
-        f"Prompt tokens: {response.usage_metadata[prompt_token_count]}
-        /nResponse tokens: {response.usage_metadata[candidates_token_count]}"
+        f"Prompt tokens: {response.usage_metadata.prompt_token_count}\nResponse tokens: {response.usage_metadata.candidates_token_count}"
     )
     
     print(response.text)
